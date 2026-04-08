63cbcef0530a2759dd1f56e8bd58e68af1eb42f2
unknown <sumxiro@gmail.com>
2026-04-08 11:44:22 -0500

    blocks face in the direction of the player, breaking particles texture doesn't work but is alright for now

--- src/main/java/com/xirosum/xiros/border/block/block/custom/BaseBorderBlock.java
+++ src/main/java/com/xirosum/xiros/border/block/block/custom/BaseBorderBlock.java
@@ -3,6 +3,7 @@ package com.xirosum.xiros.border.block.block.custom;
 import com.xirosum.xiros.border.block.block.entity.BaseBorderBlockEntity;
 import com.xirosum.xiros.border.block.block.entity.BorderBlockEntity;
 import com.xirosum.xiros.border.block.block.entity.ModBlockEntities;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
@@ -10,18 +11,31 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.screen.NamedScreenHandlerFactory;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.ItemScatterer;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 public class BaseBorderBlock extends BlockWithEntity implements BlockEntityProvider {
+    public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
+
     public BaseBorderBlock(Settings settings) {
         super(settings);
+        setDefaultState(getStateManager().getDefaultState().with(FACING, net.minecraft.util.math.Direction.NORTH));
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return getDefaultState().with(FACING, ctx.getHorizontalPlayerFacing().rotateYCounterclockwise());
     }
 
     @Override
@@ -54,5 +68,10 @@ public class BaseBorderBlock extends BlockWithEntity implements BlockEntityProvi
         }
 
         return ActionResult.SUCCESS;
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
     }
 }
