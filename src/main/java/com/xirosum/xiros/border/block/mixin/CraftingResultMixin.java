dff6314360fcd54d722724f7427cc68f797eaee6
unknown <sumxiro@gmail.com>
2026-06-03 17:43:37 -0500

    fix crash in local when crafting item

--- src/main/java/com/xirosum/xiros/border/block/mixin/CraftingResultMixin.java
+++ src/main/java/com/xirosum/xiros/border/block/mixin/CraftingResultMixin.java
@@ -8,13 +8,16 @@ import net.minecraft.screen.slot.CraftingResultSlot;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(CraftingResultSlot.class)
 public class CraftingResultMixin {
 
     @Inject(method = "onTakeItem", at = @org.spongepowered.asm.mixin.injection.At("HEAD"), cancellable = true)
     private void _onTakeItem(PlayerEntity player, ItemStack itemStack, CallbackInfo callbackInfo) {
+
+        if (player.getWorld().isClient) {
+            return;
+        }
             XirosBorderBlock.hoarder.registerNewItem(player, itemStack);
     }
 }
